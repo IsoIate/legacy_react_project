b2fdce62cd0a1f3875754d19cdf4564d8498ce4b
IsoIate <50405art@naver.com>
2021-03-24 17:49:42 +0900

    error

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -3,30 +3,20 @@ import React, {useEffect, useState} from 'react';
 import { Jumbotron, Button, Card } from "react-bootstrap";
 import { Link, Route, Switch } from "react-router-dom";
 
-import Order from './Order.js';
-import EazyOrder from './EazyOrder.js';
+import MainPage from "./MainPage";
 import '../css/App.css';
-/*import imgA from './img/Admin.png';*/
+import admin from '../img/Admin.png';
 
 function App() {
   return (
     <div className="App">
-      <Jumbotron className = "jumbo">
-          {/*<img src= { imgA } className="img"/>*/}
-          <Link as = {Link} to = { "/admin" } className = "link" >
-              {/*<div className = "adminButton"> </div>*/}
-          </Link>
-          <h1> ABC Cafe </h1>
-      </Jumbotron>
+
       <Switch>
-          <Route exact path = { "/order" }>
-              <Order/>
-          </Route>
-          <Route exact path = { "/eazyOrder" }>
-              <EazyOrder/>
+          <Route exact path = { "/mainpage" }>
+              <MainPage/>
           </Route>
           <Route exact path = {"/"}>
-              <MainPage/>
+              <Main/>
           </Route>
       </Switch>
 
@@ -34,24 +24,27 @@ function App() {
   );
 }
 
-function MainPage() {
+function Main() {
     return (
         <>
 
             <Card className="text-center" style={{height: "150vw", width: "100vw"}}>
                 <Card.Body className= " mainImg ">
+                    <div className="topBtn">
+                        <img className = "adminImg"  src={admin}/>
+                    </div>
 
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <div className="row" style={{ marginBottom: "20px" }}>
                         <div className="col-md-6">
-                            <Link as={ Link } to={ "/order" }>
-                                <Button className = "orderBtn" size="lg"> 주문하기 </Button>
+                            <Link as={ Link } to={ "/mainpage" }>
+                                <Button className = "orderBtn" size="lg"> 포장주문 </Button>
                             </Link>
                         </div>
                         <div className="col-md-6">
-                            <Link as={ Link } to={ "/eazyorder" }>
-                                <Button className = "eazyOrderBtn" size="lg"> 더 쉽게 주문하기 </Button>
+                            <Link as={ Link } to={ "/mainpage" }>
+                                <Button className = "eazyOrderBtn" size="lg"> 매장식사 </Button>
                             </Link>
                         </div>
                     </div>

5dd4b85c646b110681ce2f4ea1be00227e81a96c
IsoIate <50405art@naver.com>
2021-05-04 17:40:11 +0900

    간단 주문 페이지 생성 qr코드 모달 생성

--- src/scripts/SimpleOrder.js
+++ src/scripts/SimpleOrder.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory, useParams} from 'react-router-dom';
+import MenuBar from "./MenuBar";
+import MenuDisplay from "./MenuDisplay";
+import MenuPagingButtons from "./MenuPagingButtons";
+import OrderTable from "./OrderTable";
+import Payment from "./Payment";
 
 function SimpleOrder() {
-    console.log("simplePage")
+
+    let history = useHistory()
+    let { id } = useParams();       /* 페이지 뒤에 붙는 숫자 */
+
+    let [orderCount, orderCountChange] = useState(0);
+    let [orderPrice, orderPriceChange] = useState(0);
+
+    let [pageNum] = useState(1);
+
     return (
         <>
             <div>
-                <h1> SimpleOrder 페이지 </h1>
-                <p>dfkjaldfja</p>
+                {/* 메뉴 바 모듈화 */}
+                <MenuBar history = { history } />
+            </div>
+            {/* 메뉴 선택 버튼 모듈화 */}
+            <div className="container-fluid ">
+                <div className="row menuSelectDiv">
+                    <MenuDisplay orderCountChange = { orderCountChange } orderPriceChange = { orderPriceChange }
+                                 id = { id } history = { history } pageNum = { pageNum } />
+                </div>
+            </div>
+
+            {/* 음료 이동 버튼 */}
+            <MenuPagingButtons />
+
+            {/* footer */}
+            <div className="footer">
+                {/* 주문내역 테이블 */}
+                <OrderTable />
+
+                {/* 주문정보 창 */}
+                <Payment orderCount = { orderCount } orderPrice = { orderPrice } />
             </div>
         </>
     );

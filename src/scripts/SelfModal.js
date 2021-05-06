c846512fdeeee9b67cd9d4fa1cc00d35b1a61bca
IsoIate <50405art@naver.com>
2021-05-06 17:07:33 +0900

    왜안되는지모르겠네

--- src/scripts/SelfModal.js
+++ src/scripts/SelfModal.js
@@ -1,37 +1,43 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import { useHistory } from 'react-router-dom'
 import { Modal, Button} from "react-bootstrap"
 import qrcodeImg from '../img/qrcodeImg.png'
 import '../css/QRModal.css'
 
-function QRModal(props) {
+function SelfModal(props) {
 
     let history = useHistory();
 
+    useEffect(() => {
+
+    })
+
     return (
         <>
-            <Modal show = { props.show } onHide = { props.onHide } >
+            <Modal selfShow = { props.selfShow } onHide = { props.selfHide } >
                 <Modal.Header>
                     <div className = "headerDiv">
-                        <h2> 전자출입명부 작성 </h2>
+                        <h2> 직접 출입명부 작성 </h2>
                     </div>
                 </Modal.Header>
                 <Modal.Body>
                     <div className = "qrBody">
-                        <h3> 전자출입명부 작성을 위해 <br/>QR코드를 스캔해 주세요 </h3>
-                        <img className = "qrcode" src = { qrcodeImg }/>
-                        <h3> ABC 카페 </h3>
+                        <h3> 오늘 날짜를 작성해 주세요 </h3>
+
+                    </div>
+                    <div>
+                        <h3> 지역구를 작성해 주세요 </h3>
                     </div>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="primary" onClick={() => {
                         history.push("./MainPage/0")
                     }}>
-                    작성 완료 </Button>
+                        작성 완료 </Button>
                 </Modal.Footer>
             </Modal>
         </>
     )
 }
 
-export default QRModal;
+export default SelfModal;
